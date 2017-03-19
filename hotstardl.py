9bee1e53ff80f29a7f1f4bf68d126178efa68e69
girish946 <girish946@gmail.com>
2017-03-20 00:06:05 +0530

    argparser added

--- hotstardl.py
+++ hotstardl.py
@@ -1,14 +1,28 @@
+import sys
 import requests
 import time
 import json
 import os
-from clint.textui import prompt
 import const
+import argparse
+from clint.textui import prompt
 from lib import err, info
 
+def getVideoId(url):
+    if "hotstar.com/" in url:
+        video_id = url.split("/")[-1]
+        if len(video_id) == 10  and int(video_id):
+            return video_id
+        else:
+            return
+    elif len(url) == 10 and int(url):
+        return url
+    else:
+        return
+        
 
-def main():
-    video_id = prompt.query("Enter the video ID: ")
+
+def getVideoFileUrl(video_id):
     title_url = const.BASEURL.format(video_id)
     video_resp = requests.get(title_url, headers=const.HEADERS)
     if video_resp.status_code != requests.codes.ok:
@@ -32,9 +46,10 @@ def main():
     cdn_content = cdn_resp.json()['resultObj']
     src = cdn_content['src']
 
-    file_url = src.replace("https", "hlsvariant://https")
+    file_url = src.replace("http", "hlsvariant://http")
     file_url = file_url.replace("2000,_STAR.", "2000,3000,4500,_STAR.")
-
+    info(file_url)
+    
     if (vid_title):
         localFile = str(vid_title)
     if (vid_episode):
@@ -42,24 +57,66 @@ def main():
     if (vid_ep_title):
         localFile += "_" + str(vid_ep_title)
     localFile += ".mp4"
+    localFile = localFile.replace("'", '-')
+    localFile = localFile.replace('"', '-')
+    localFile = localFile.replace('(', '-')
+    localFile = localFile.replace(')', '-')
+    return file_url, localFile
+    
 
-    info("Downloading with filename: {0}".format(localFile))
-    video_options = "1080p (best), 180p (worst), 234p, 360p, 404p, 720p, 900p"
-    quality = prompt.query("Enter quality {0}: ".format(video_options))
+def main(video_id=None, quality=None, saveOrStream='D'):
 
-    choice = prompt.query("Enter 'S' to Stream and 'D' to Download ")
+    if not video_id:
+        idstr = prompt.query("Enter the video ID: ")
+        video_id = getVideoId(idstr)
+        if not video_id:
+            err('invalid video id entered')
+            return
+    try:
+        file_url, localFile = getVideoFileUrl(video_id)
+    
+        info("Downloading with filename: {0}".format(localFile))
+    
+        if not quality:
+            video_options = "1080p (best), 180p (worst), 234p, 360p, 404p, 720p, 900p"
+            quality = prompt.query("Enter quality {0}: ".format(video_options))
+        
+        if not saveOrStream:
+            choice = prompt.query("Enter 'S' to Stream and 'D' to Download ")
+        else:
+            choice = saveOrStream    
 
-    command = "livestreamer \"{0}\" \"{1}\" ".format(file_url, quality)
-    if choice == "D" or choice == 'd':
-        command += "-o {0}".format(localFile)
-    elif choice == "S" or choice == 's':
-        info("Streaming ...")
-    else:
-        err("Invalid command")
-        return
+        command = "livestreamer \"{0}\" \"{1}\" ".format(file_url, quality)
+        if choice == "D" or choice == 'd':
+            command += "-o \"{0}\"".format(localFile)
+        elif choice == "S" or choice == 's':
+            info("Streaming ...")
+        else:
+            err("Invalid command")
+            return
 
-    info("Starting system command : {0}".format(command))
-    os.system(command)
-
+        info("Starting system command : {0}".format(command))
+        os.system(command)
+    except Exception as e:
+        err(str(e))
+        
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) <2:
+        main()
+    else:
+        parser = argparse.ArgumentParser(description='tool to stream and \
+        download Hotstar videos')
+
+        parser.add_argument('-u', '--url', default=None, help='video url or id')
+        parser.add_argument('-q','--quality',  default=None,
+                            help='video quality. 1080p (best), 180p (worst),\
+                             234p, 360p, 404p, 720p, 900p')
+        parser.add_argument('-c', '--choice',  default=None,
+                            help="'D' for downloading 'S' for streaming")
+
+        args = parser.parse_args()
+        if args.url:
+            video_id = getVideoId(args.url)
+        else:
+            video_id = None
+        main(video_id=video_id, quality=args.quality, saveOrStream=args.choice)
